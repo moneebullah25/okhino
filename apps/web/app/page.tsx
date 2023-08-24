bd2a3945e29fa8a0eb10f1ade94541ed93b8d3e6
Muneeb Ullah <moneebullah25@gmail.com>
2023-08-24 19:35:09 +0500

    created web app

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div className='text-8xl bg-amber-500'>
+      Hello World Nextjs 13
+    </div>
+  );
+}
